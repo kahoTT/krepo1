968a32f39691595494df6938b7b24effa13a2856
Kaho Tse <kaho.tse@monash.edu>
2020-09-23 10:08:02 +1000

    update

--- kshot.py
+++ kshot.py
@@ -108,7 +108,6 @@ class Shot(object):
             d0n = d0 - dd0  
             d0 = np.minimum(GOLDEN * d0, np.maximum(d0 / GOLDEN, d0n))  # 1.61 , d0 is the boundary density 
 ### 1st zone        
-
         p_surf = p0
         t_surf = T
         #dm = d0 * 4 * np.pi * (R**3 - (R-dr)**3) / 3  # solve directly , dm : change of mass
@@ -122,8 +121,6 @@ class Shot(object):
         p1 = p_surf + 0.5 * xm0 * g0 / (4 * np.pi * r0**2) 
         u1 = u0
         d1 = d0
-#        u1bt1 = u0bt0
-#        u1bd1 = u0bd0
         while True:
             p0,u0,p0bt0,p0bd0,u0bt0,u0bd0 = eos(t0 , d0)   # what is b? 
             ki0,ki0bt0,ki0bd0 = kappa(t0 , d0) 
@@ -173,17 +170,17 @@ class Shot(object):
 # 1st zone mass is modified by not affecting all the variables fro which the original zone mass is used
 
 #        k = np.log10(1 - (M - xm_surf)/xms + xmsf * (M - xm_surf) / xms) / np.log10(xmsf) -1
-        nmax = 2**11 
-        tn  = np.ndarray(nmax+1)
-        dn  = np.ndarray(nmax+1)
-        xm  = np.ndarray(nmax+1)
-        pn  = np.ndarray(nmax+1)
+        k   = 1000 
+        tn  = np.ndarray(k+1)
+        dn  = np.ndarray(k+1)
+        xm  = np.ndarray(k+1)
+        pn  = np.ndarray(k+1)
 
-        xln = np.ndarray(nmax)
-        sn  = np.ndarray(nmax)
-        rn  = np.ndarray(nmax)
-#        zn  = np.ndarray(num_run) 
-#        gn  = np.ndarray(num_run) 
+        xln = np.ndarray(k)
+        sn  = np.ndarray(k)
+        rn  = np.ndarray(k)
+#        zn  = np.ndarray(k) 
+#        gn  = np.ndarray(k) 
 
         tn[0]  = t_surf
         dn[0]  = d1
@@ -191,15 +188,15 @@ class Shot(object):
         pn[0]  = p1
         xln[0] = xl0
         sn[0]  = s0
+        rn[0]  = R
 
         tn[1]  = t0
         dn[1]  = d0
         xm[1]  = xm0
         pn[1]  = p0
-        rn[1]  = R
 
 # starting from the second zone
-        for j in range(nmax):
+        for j in range(1 , k , 1):
             if d0 > 5e11 or t0 > 5e9:
                 break
             xm2 = xm1
@@ -219,53 +216,65 @@ class Shot(object):
             t1  = t0
             d2  = d1
             d1  = d0
+            u2  = u1
             u1  = u0    
             xl1 = xl0
             s1  = s0
-            p   = p1 +  xm0 * g0 / (4 * np.pi * r0**2) 
-            dmx = 2 * mdot / (xm0 + xm1)
+            p   = p1 +  0.5 * (xm0 + xm1) * g0 / (4 * np.pi * r0**2) 
+            dmx1 = 2 * mdot / (xm1 + xm2)
+            dmx0 = 2 * mdot / (xm0 + xm1)
             dt0 = xm0 / mdot
-            pdv1 = p2 * (d1 - d2) / d2**2
+            pdv1 = 0.5 * (p2 + p1) * (1 / d2 - 1 / d1)
+            du1 = u2 - u1
+            dL1 = (du1 + pdv1) * dmx1
+
             while True:
                 p0,u0,p0bt0,p0bd0,u0bt0,u0bd0 = eos(t0 , d0)  
                 ki0,ki0bt0,ki0bd0 = kappa(t0 , d0) 
     
-                pdv0 = p1 * (d0 - d1) / d1**2
-                du   = u0 - u1
-                sdot = du * dmx
-                dL   = (sdot + (pdv1 - pdv0) / dt0) * d1  # mass from zone 1 to zone 2, so * d1
+                pdv0    = 0.5 * (p1 + p0) * (1 / d0 - 1 / d1)
+                pdv0bt0 = 0.5 * p0bt0 * (1 / d0 - 1 / d1)
+                pdv0bd0 = 0.5 * p0bd0 * (1 / d0 - 1 / d1) - 0.5 * (p1 + p0) * (1 / d0**2)
+                
+                du0    = u1 - u0
+                du0bt0 = - u0bt0
+                du0bd0 = - u0bd0
+
+                dL0  = (du0 + pdv0) * dmx0
+                dL   = (dL1 + dL0 + s1) * xm1
                 xl0  = xl1 - dL
 
-                rm = np.cbrt(r0**3 - 3 * xm0 / (4 * np.pi * d0))
-                dr0 = r0 - rm
+                rc = np.cbrt(r0**3 - 3 * 0.5 * xm0 / (4 * np.pi * d0))
+                dr0 = r0 - rc
                 ac = 4 * np.pi * r0**2 * ARAD * CLIGHT / 3
-                acdr0 = ac * ki0 / (d0 * 0.5 * dr0)
+#                acdr0 = ac * ki0 / (d0 * 0.5 * dr0)
+                acdr0 = ac * ki0 / (d0 * dr0)
                 l0 = (t0**4 - t1**4) * acdr0
-    
                 f0 = p0 - p
                 h0 = l0 - xl0
     
+                breakpoint () #    
                 b = np.array([f0,h0])
                 b1 = np.array([p,xl0])
     
-                if np.abs(f0/p) < 1e-12 and np.abs(h0/xl0) < 1e-12:
+                if np.abs(f0/p) < 1e-12 and np.abs(h0/xl1) < 1e-12:
                     break
-    
-                dr0bd0 = - xm0 / (rm**2 * 4 * np.pi * d0**2)
+                dxl0bt0 = - (du0bt0 + pdv0bt0) * dmx0 * xm1
+                dxl0bd0 = - (du0bd0 + pdv0bd0) * dmx0 * xm1
+                dr0bd0 = - 0.5 * xm0 / (rc**2 * 4 * np.pi * d0**2)
                 f0bt0 = p0bt0
                 f0bd0 = p0bd0 
     
-                h0bt0 = l0 * 2 * ki0bt0 + acdr0 * 4 * t0 ** 3
-                h0bd0 = l0 * (2 * ki0bd0 - 1 / d0 - dr0bd0 / dr0)
+                h0bt0 = l0 * 2 * ki0bt0 + acdr0 * 4 * t0 ** 3 - dxl0bt0
+                h0bd0 = l0 * (2 * ki0bd0 - 1 / d0 - dr0bd0 / dr0) - dxl0bd0
     
                 A = np.array([[f0bt0, f0bd0],[h0bt0, h0bd0]])
                 c = np.linalg.solve(A,b)
                 v = np.array([t0, d0])
                 t0, d0 = v - c
 
-            dt0 = xm0 / mdot
             s0  = net.epsdt(t0, d0, dt0, update=True)
-            dL  = s0 * xm0
+            rm  = np.cbrt(r0**3 - 3 * xm0 / (4 * np.pi * d0))
 
             tn[j+1]  = t0
             dn[j+1]  = d0
@@ -279,13 +288,11 @@ class Shot(object):
             print(f'current zone mass={xm0:12.5e}, next zone mass={xm0*xmsf:12.5e}')
 
         self.xm  = xm[:j+1]
+        self.pn  = pn[:j+1]
+        self.tn  = tn[:j+1]
+        self.dn  = dn[:j+1]
         self.xln = xln[:j]
         self.rn  = rn[:j]
 
         self.y = np.cumsum((self.xm[1:] / (4 * np.pi * self.rn**2)))
-
-#        self.y_m = np.zeros(jm + 2)
-#        self.y_m[1:-1] = 0.5 * (self.y[:-2] + self.y[1:-1])
-#        self.y_m[ 0] = self.y[0]
-#        self.y_m[-1] = self.y[-2]
 
