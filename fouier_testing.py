69806030eccb14ff18fb219a2269d2b0c544cfa7
kahoTT <kaho.tse@monash.edu>
2022-07-05 16:24:45 +1000

    test how data gap affect power

--- fouier_testing.py
+++ fouier_testing.py
@@ -2,21 +2,47 @@ import numpy as np
 import stingray
 import matplotlib.pyplot as plt
 
-t = np.arange(0, 1000, 0.1)
-y1 = np.random.uniform(-1, 1, size=len(t))
-y = y1 - 20
-lc = stingray.Lightcurve(t, y, input_counts=False, skip_checks=True)
-spec = stingray.Powerspectrum(lc, norm='None')   
-spec.power = abs(spec.power)
-fig, ax = plt.subplots(2)
-fig = fig
-ax = ax
-ax[0].plot(t,y)
-ax[1].plot(spec.freq, spec.power)
-plt.xscale('log')
-plt.yscale('log')
-plt.ylabel('Abs power')
-plt.ylabel('Abs power')
-plt.show()
+class TestLc(object):
+	def __init__(self):
+		t = np.arange(0, 1000, 0.1)
+		y1 = np.random.uniform(-1, 1, size=len(t))
+		y = y1 + 20
+		lc = stingray.Lightcurve(t, y, input_counts=False, skip_checks=True)
+		spec = stingray.Powerspectrum(lc, norm='None')   
+		power = abs(spec.power)
+		sum_pow = sum(power)
+		self.t = t
+		self.y = y
+		self.freq1 = spec.freq
+		self.pow1 = power
+
+		fig, ax = plt.subplots(4, figsize=(8,10))
+		self.fig = fig
+		self.ax = ax
+		ax[0].plot(t,y)
+		ax[0].set_xticklabels([])
+		ax[1].plot(spec.freq, power)
+		ax[1].set_xscale('log')
+		ax[1].set_yscale('log')
+		ax[1].set_ylabel('Abs power')
+		ax[1].set_title(f'Power Sum = {sum_pow:.2f}')
+		
+
+		y2 = y
+		y2[2000:9000] = 20
+
+		lc2 = stingray.Lightcurve(t, y2, input_counts=False, skip_checks=True)
+		spec2 = stingray.Powerspectrum(lc2, norm='None')   
+		power2 = abs(spec2.power)
+		sum_pow2 = sum(power2)
+		self.freq2 = spec2.freq
+		self.pow2 = power2
 
 
+		ax[2].plot(t,y2)
+		ax[2].set_xticklabels([])
+		ax[3].plot(spec2.freq, power2)
+		ax[3].set_xscale('log')
+		ax[3].set_yscale('log')
+		ax[3].set_ylabel('Abs power')
+		ax[3].set_title(f'Power Sum2 = {sum_pow2:.2f}, sum1/sum2 = {sum_pow/sum_pow2:.2f}')
