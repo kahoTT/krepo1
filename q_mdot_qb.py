e71df2b93507a0f34f448061f657080f074a8860
kahoTT <kaho.tse@monash.edu>
2022-12-13 13:48:09 +1100

    update starshot paper plot module

--- q_mdot_qb.py
+++ q_mdot_qb.py
@@ -14,22 +14,31 @@ for i in _allp:
     q.extend(p.Q)
     mdot.extend(p.mdot)
     qb.extend(p.Qb)
-# mdot = [mdot for _,mdot in sorted(zip(q, mdot))]
-# qb = [qb for _,qb in sorted(zip(q, qb))]
-# q = [q for _,q in sorted(zip(mdot, q))]
-# qb = [qb for _,qb in sorted(zip(mdot, qb))]
 
-a = np.unique(q)
-b = np.unique(mdot)
-qqb = np.array(qb).reshape(len(b), len(a), order='F')
+# drop repeat runs
+_stack = np.vstack(q, mdot)
+_, _ind = np.unique(_stack, axis=1, return_index=True)
+sortind = _ind.sort()
+q = q[sortind]
+mdot = mdot[sortind]
+qb = qb[sortind]
+
+qb2 = [qb2 for _,qb2 in sorted(zip(q, qb))]
+sortqb = [sortqb for _,sortqb in sorted(zip(mdot, qb2))]
+sortq = q.sort()
+sortmdot = mdot.sort()
+
+a = np.unique(sortq)
+b = np.unique(sortmdot)
+qqb = np.array(sortqb).reshape(len(b), len(a), order='F')
 
 fig, ax = plt.subplots()
-pcm = ax.pcolor(a, b, qqb)
+pcm = plt.pcolor(a, b, qqb)
 
 ax.set_xlabel('Surface luminosity / $\dot{m}$ (MeV/u)', fontsize=15)
 ax.set_ylabel('$\dot{m}_{\mathrm{Edd}}$', fontsize=15)
 ax.tick_params(labelsize=13)
 cbar = plt.colorbar(pcm)
 cbar.set_label('Base luminosity / $\dot{m}$ (MeV/u)', rotation=270, fontsize=15, labelpad=20)
-cbar.update_ticks(size=15)
+# cbar.update_ticks(size=15)
 plt.tight_layout()
