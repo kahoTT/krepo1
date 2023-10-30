be8a57be3dba0f5f9f39c59aba592694000ac3b9
kahoTT <kaho.tse@monash.edu>
2023-10-30 21:25:40 +1100

    finished making prediction

--- AE_capital/task.py
+++ AE_capital/task.py
@@ -55,7 +55,7 @@ def mw(t, t_current, wsize=None):
     """
     if wsize is None:
         wsize = 10
-    _int  = np.where((0 < (t - t_current)) & ((t - t_current) < wsize))
+    _int  = np.where((0 < (t_current - t)) & ((t_current - t) < wsize))
     return _int
 
 def predict(p):
@@ -64,6 +64,11 @@ def predict(p):
     pred = _sum1 - _sum2
     return pred
 
+def findstart(t, wsize):
+    """Find the index for start of prediction."""
+    _int = np.where(np.argmin(abs(t - wsize)))
+    return _int
+
 class PredictCurve(object):
     """Predict prices using moving average approach
     
@@ -71,19 +76,40 @@ class PredictCurve(object):
     def __init__(self, path=path, wsize=10):
         a = open(path)
         t = a[:, 0]
+        t0 = t[0]
         if check(t=t):
             raise AttributeError('Time resolution is not consistent')
+        else:
+            dt = t[1] - t[0]
         p1 = a[:, 1]
         p2 = a[:, 2]
         logp1, lt = logdata(p=p1, t=t)
         logp2, _ = logdata(p=p2)
         clt1, clp1 = clean(lt, logp1)
         clt2, clp2 = clean(lt, logp2)
-        for i in (t-t[0]):
-            if i + wsize > t[-1]:	# loop stops if the window covers inexsit time
-                break
-            
+        list_p1 = []
+        list_p2 = []
 
+	# First time series
+        for i in (clt1-t0):
+            if i > (clt1-t0)[-1]:	# loop stops if the window covers inexsit time
+                break
+            _startind1 = findstart(t=clt1, wsize=wsize)
+            _int1 = mw(t=clt1[_startind1:], t_current=i, wsize=wsize)
+            pred1 = predict(clp1[_int1])
+            list_p1.append(pred1)
+            predt1 = np.append(clt1[_int1], (clt1[-1]+dt))
+
+        # Second time series
+        for j in (clt2-t0):
+            if j > (clt2-t0)[-1]:	# loop stops if the window covers inexsit time
+                break
+            _startind2 = findstart(t=clt2, wsize=wsize)
+            _int2 = mw(t=clt2[_startind2], t_current=i, wsize=wsize)
+            pred2 = predict(clp2[_int2])
+            list_p2.append(pred2)
+            predt2 = np.append(clt2[_int2], (clt2[-1]+dt))
+            
         self.a = a
         self.p1 = p1
         self.p2 = p2
