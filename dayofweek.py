7f76060a4c56f4d23ad6a2e320281aeb3a087074
kahoTT <kaho.tse@monash.edu>
2024-10-17 22:44:56 +1100

    try tc = o(1)

--- dayofweek.py
+++ dayofweek.py
@@ -1,3 +1,25 @@
 class Dow:
-    def dayofweek(day=17, month=10, year=2024):
+    def dayofweek(day, month, year):
+        to_day = 17
+        to_month = 10
+        to_year = 2024
+        to_dow = 4
+        dict_dow = {0: "Sunday", 1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday"}
+        nums_month = [31,28,31,30,31,30,31,31,30,31,30,31]
 
+
+        for y in
+        # if month > to_month:
+        #     diff_month = sum(nums_month[to_month-1:month-1])
+        # else:
+        #     diff_month = -sum(nums_month[month-1:to_month-1])
+
+        # diff_year = year - to_year
+        # if (diff_year%4 == 0 and diff_year%100 != 0) or diff_year%400 == 0:
+        #     if diff_year < 0 and 
+
+
+        # diff_day = (diff_year)*365 + (diff_month) + (day - to_day) + to_dow
+        # print(diff_day)
+
+        # return dict_dow[(diff_day)%7]
